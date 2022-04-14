a73c91b414ab6bbc8e359d1eba2e3e9c8c38b2bf
Ben Hessel <benhessel@Bens-MacBook-Air.local>
2022-04-14 14:16:48 -0400

    extensions added, everything seems to be working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Pageone from './comopnents/pageOne.jsx'
 
 function App() {
   return (
     <div className="App">
       <h1>localhost live</h1>
+      <Pageone />
+       
     </div>
   );
 }
